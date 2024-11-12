619295356225a0d5e00268a13b291dafdffc263b
Weizhao Ouyang <o451686892@gmail.com>
2024-11-12 23:08:19 +0800

    mods: add BusTicketPrice v1.0.0
    Signed-off-by: Weizhao Ouyang <o451686892@gmail.com>

---
+++ BusTicketPrice/BusTicketPrice/ModConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BusTicketPrice
+{
+	public class ModConfig
+	{
+		public int DesertBusTicketPrice { get; set; }
+
+        public ModConfig()
+		{
+			this.DesertBusTicketPrice = 2;
+		}
+
+        public bool IsValid()
+        {
+            return DesertBusTicketPrice >= 0 && DesertBusTicketPrice <= 999999;
+        }
+	}
+}
+
---
+++ BusTicketPrice/BusTicketPrice/ModEntry.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Reflection;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewValley;
+using StardewValley.Locations;
+
+namespace BusTicketPrice
+{
+    internal sealed class ModEntry : Mod
+    {
+        private ModConfig Config = null!;
+
+        public override void Entry(IModHelper helper)
+        {
+            // Load config file
+            this.Config = this.Helper.ReadConfig<ModConfig>();
+            
+            // Validate config
+            if (this.Config.DesertBusTicketPrice < 0)
+            {
+                this.Monitor.Log("Price cannot be negative. Setting to default value of 2.", LogLevel.Warn);
+                this.Config.DesertBusTicketPrice = 2;
+                this.Helper.WriteConfig(this.Config);
+            }
+
+            helper.Events.GameLoop.DayStarted += this.OnDayStarted;
+            helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
+        }
+
+        private void OnSaveLoaded(object? sender, SaveLoadedEventArgs e)
+        {
+            // Set price immediately when save is loaded
+            SetBusPrice();
+        }
+
+        private void OnDayStarted(object? sender, DayStartedEventArgs e)
+        {
+            // Set price at start of each day
+            SetBusPrice();
+        }
+
+        private void SetBusPrice()
+        {
+            if (!Context.IsWorldReady)
+                return;
+
+            if (Game1.getLocationFromName("BusStop") is BusStop busStop)
+            {
+                busStop.TicketPrice = this.Config.DesertBusTicketPrice;
+                this.Monitor.Log($"Bus ticket price set to {busStop.TicketPrice}g", LogLevel.Debug);
+            }
+            else
+            {
+                this.Monitor.Log("Could not find BusStop location", LogLevel.Warn);
+            }
+        }
+    }
+}
